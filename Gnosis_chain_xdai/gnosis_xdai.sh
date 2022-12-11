c5b15f5ce7b8c10d5d81392a6a2234023440fdb1
Mustafa AKSOZ <m-aksoz@hotmail.com>
2022-12-11 20:27:58 +0300

    Gnosis chain xdai update

---
+++ Gnosis_chain_xdai/gnosis_xdai.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+echo -e '\e[0m'                                                              
+echo -e '@@@  @@@   @@@@@@   @@@@@@@   @@@@@@@@    @@@   @@@@@@@@     @@@'
+echo -e '@@@@ @@@  @@@@@@@@  @@@@@@@@  @@@@@@@@   @@@@  @@@@@@@@@@   @@@@'
+echo -e '@@!@!@@@  @@!  @@@  @@!  @@@  @@!       @@@!!  @@!   @@@@  @@@!!'
+echo -e '!@!!@!@!  !@!  @!@  !@!  @!@  !@!         !@!  !@!  @!@!@    !@!'
+echo -e '@!@ !!@!  @!@  !@!  @!@  !@!  @!!!:!:     @!@  @!@ @! !@!    @!@'
+echo -e '!@!  !!!  !@!  !!!  !@!  !!!  !!!!!::     !@!  !@!!!  !!!    !@!'
+echo -e '!!:  !!!  !!:  !!!  !!:  !!!  !!:         !!:  !!:!   !!!    !!:'
+echo -e ':!:  !:!  :!:  !:!  :!:  !:!  :!:         :!:  :!:    !:!    :!:'
+echo -e ':::   ::   ::::::   :::::::   :::::::     :::   ::::::::     :::'
+echo -e '\e[0m'
+
+
+sudo apt-get update && sudo apt-get install libsnappy-dev libc6-dev libc6 librocksdb5.17 unzip -y
+
+wget https://github.com/NethermindEth/nethermind/releases/download/1.14.7/nethermind-linux-arm64-1.14.7-4fe81c6-20221130.zip
+
+sleep 1 
+
+unzip nethermind-linux-arm64-1.14.7-4fe81c6-20221130.zip -d nethermind
+
+sleep 1
+
+cd nethermind
+
+./Nethermind.Launcher

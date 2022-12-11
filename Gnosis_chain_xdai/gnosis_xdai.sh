8bdf2a89fc979fa77746f7ed8fbca96b0b83df5d
Mustafa AKSOZ <m-aksoz@hotmail.com>
2022-12-11 20:53:20 +0300

    update

--- Gnosis_chain_xdai/gnosis_xdai.sh
+++ Gnosis_chain_xdai/gnosis_xdai.sh
@@ -13,13 +13,15 @@ echo -e ':::   ::   ::::::   :::::::   :::::::     :::   ::::::::     :::'
 echo -e '\e[0m'
 
 
-sudo apt-get update && sudo apt-get install libsnappy-dev libc6-dev libc6 librocksdb5.17 unzip -y
 
-wget https://github.com/NethermindEth/nethermind/releases/download/1.14.7/nethermind-linux-arm64-1.14.7-4fe81c6-20221130.zip
+sudo apt-get update && sudo apt-get install libsnappy-dev libc6-dev libc6 unzip
+
+
+wget https://github.com/NethermindEth/nethermind/releases/download/1.14.7/nethermind-linux-amd64-1.14.7-4fe81c6-20221130.zip
 
 sleep 1 
 
-unzip nethermind-linux-arm64-1.14.7-4fe81c6-20221130.zip -d nethermind
+unzip nethermind-linux-amd64-1.14.7-4fe81c6-20221130.zip -d nethermind
 
 sleep 1
 
